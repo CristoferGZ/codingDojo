c5db27d965d06caf8bb089f119b987411114aaeb
CristoferGZ <cristofer.gutierrez.cerda@alumnos.sip.cl>
2022-04-07 12:08:55 -0400

    cambio 1

---
+++ Gutierrez_Cristofer_prediccion.js
@@ -0,0 +1,21 @@
+function myBirthYearFunc() {
+    console.log("Nací en " + 1980);
+}
+//indica el año en el que naci //
+
+function myBirthYearFunc(EntradaAñoNacimiento) {
+    console.log("Nací en " + EntradaAñoNacimiento);
+}
+//naci en 1980//
+
+function add(num1, num2) {
+    console.log("¡Sumando números!");
+    console.log("num1 is: " + num1);
+    console.log("num2 is: " + num2);
+    var sum = num1 + num2;
+    console.log(sum);
+}
+
+//console.log indica que num 1= a 10 y //
+//num2 = a 20 por lo tanto la suma de estos 
+//nos daria como resultado 30//
