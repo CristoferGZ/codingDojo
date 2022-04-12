3a656ad060ff12d10b68ee732d75caa0957428a4
CristoferGZ <cristofer.gutierrez.cerda@alumnos.sip.cl>
2022-04-12 10:53:25 -0400

    diagramaenT

---
+++ diagrama..js
@@ -0,0 +1,44 @@
+/*
+Fragmento de código 1
+//
+*/
+
+function muestraInformaciónContacto() {
+    var auntContactInfo = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+    console.log(auntContactInfo);
+}
+
+/* 
+     funcion muestra informacion de contacto
+   variable            datos 
+   auntContactInfo   = ["Paula", "Smith", "Calle Principal 1234", "St. Louis", "MO", 12345];
+
+Fragmento de código 2
+*/
+function muestraListaDeCompras() {
+    var produce = ["manzanas", "naranjas"];
+    var frozen = ["brócoli", "helado"];
+    frozen.push("croqueta de papa");
+    console.log(frozen);
+}
+/*
+   funcion  muestra lista de compras 
+    variable                     datos
+   produce              =        ["manzanas", "naranjas"];
+   frozen               =        ["brócoli", "helado"];   
+   frozen               =        ("croqueta de papa");
+
+
+Fragmento de código 3
+*/
+
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zoro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
+   /* 
+    funcion 
+     variable             datos
+      movieLibrary              ["Bambi", "E.T.", "Toy Story"];
+    */
